acafcfa0b3335566e724b1c62cf6f704ad3bf8ea
GhufronAndriansyah <ghufron_andriansyah_27rpl@student.smktelkom-mlg.sch.id>
2020-11-17 14:15:58 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import './App.css';
+import List from './List';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/List" className="nav-link">List</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/List">
+          <List />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
